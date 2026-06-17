b300a099645b1223d6afa9bf2a33a52128f85d60
Paulina S. <paulina.serrano@outlook.com>
2026-06-16 20:14:04 -0600

    v 1.3

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.header("Relación de datos de carros")
 st.write("*Se está trabajando en el tablero estadístico...🛠️💻*")
 
 # Agregar métricas
-st.metric("Carros disponibles", 51525, int, "red")
+st.metric("Carros disponibles", "51,525", None, "red")
 
 
 # Crear borón para histograma
@@ -19,6 +19,8 @@ hist_button = st.button('Construir histograma')
 
 
 if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     # Crear un histograma utilizando plotly.graph_objects
     # Se crea una figura vacía y luego se añade un rastro de histograma
     fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
@@ -26,5 +28,6 @@ if hist_button:
     # Opcional: Puedes añadir un título al gráfico si lo deseas
     fig.update_layout(title_text='Distribución del Odómetro')
 
-    # Mostrar el gráfico Plotly
-    fig.show()
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
