cf1b8f5195b31f06aa81dbc3486fbc36d6fa5cba
Paulina S. <paulina.serrano@outlook.com>
2026-06-16 20:05:48 -0600

    Versión 1

---
+++ app.py
@@ -0,0 +1,30 @@
+# Cargar librerías
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+
+# Leer datos de csv
+car_data = pd.read_csv('vehicles_us.csv')
+
+# Crear encabezado y texto de ayuda
+st.header("Relación de datos de carros")
+st.write("*Se está trabajando en el tablero estadístico...🛠️💻*")
+
+# Agregar métricas
+st.metric("Carros disponibles", 51525, int, "red")
+
+
+# Crear borón para histograma
+hist_button = st.button('Construir histograma')
+
+
+if hist_button:
+    # Crear un histograma utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de histograma
+    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Distribución del Odómetro')
+
+    # Mostrar el gráfico Plotly
+    fig.show()
